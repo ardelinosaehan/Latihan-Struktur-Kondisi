f99b73a50b0b4a7b2763020872df095c03f195bb
unknown <saehanardelino@gmail.com>
2021-11-09 19:16:18 +0700

    Menambahkan Program dan SS

---
+++ sklatihan1.py
@@ -0,0 +1,9 @@
+a = int(input('Masukkan nilai a:  '))
+b = int(input('Masukkan nilai b:  '))
+
+if a > b :
+    print("a adalah nilai terbesar")
+          
+elif b > a :
+    print("b adalah nilai terbesar")
+          
---
+++ sklatihan2.py
@@ -0,0 +1,6 @@
+data = []
+for i in range (3):
+    x = int(input('Masukkan bilangan:  '))
+    data.append(x)
+list.sort(data)    
+print(data)    
